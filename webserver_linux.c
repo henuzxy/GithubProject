faf74a07f6e09e9a12afa67ea392664acf41dbd1
henuzxy <1961714414@qq.com>
2019-07-20 02:46:07 -0700

    webserver

--- webserver_linux.c
+++ webserver_linux.c
@@ -8,12 +8,12 @@
 
 #define BUF_SIZE (1024)
 #define SMALL_BUF (128)
-
+#define true (1)
 void* request_handler(void* arg);
 void send_data(FILE* fp,char* ct,char* file_name);
 char* content_type(char* file);
 void send_error(FILE* fp);
-void error_handling(char* message);
+void error_handling(const char* message);
 
 int main(int argc,char* argv[]){
     int serv_sock,clnt_sock;
@@ -38,7 +38,7 @@ int main(int argc,char* argv[]){
     while(true){
         clnt_adr_size = sizeof(clnt_adr);
         clnt_sock = accept(serv_sock,(struct sockaddr*)&clnt_adr,&clnt_adr_size);
-        fprintf(stdout,"Connection Request:%s:%d\n",inet_ntoa(clnt_adr.sin_addr),ntohs(clnt_adr,sin_port));
+        fprintf(stdout,"Connection Request:%s:%d\n",inet_ntoa(clnt_adr.sin_addr),ntohs(clnt_adr.sin_port));
         pthread_create(&t_id,NULL,request_handler,&clnt_sock);
         pthread_detach(t_id);
     }
@@ -56,8 +56,87 @@ void* request_handler(void *arg){
     char ct[15];
     char file_name[30];
     clnt_read = fdopen(clnt_sock,"r");
-    clnt_write = fdopen(dup(clnt),"w");
+    clnt_write = fdopen(dup(clnt_sock),"w");
+    fgets(req_line,SMALL_BUF,clnt_read);
+    if(strstr(req_line,"HTTP/") == NULL){
+        send_error(clnt_write);
+        fclose(clnt_read);
+        fclose(clnt_write);
+        return NULL;
+    }
+    strcpy(method,strtok(req_line," /"));
+    strcpy(file_name,strtok(NULL," /"));
+    strcpy(ct,content_type(file_name));
+    if(strcmp(method,"GET") != 0){
+        send_error(clnt_write);
+        fclose(clnt_read);
+        fclose(clnt_write);
+        return NULL;
+    }
 
+    fclose(clnt_read);
+    send_data(clnt_write,ct,file_name);
+    return NULL;
+}
 
+void send_data(FILE* fp,char* ct,char* file_name){
+    char protocol[] = "HTTP/1.0 200 ok\r\n";
+    char server[] = "Server:Linux Web Server \r\n";
+    char cnt_len[] = "Content-length:2048\r\n";
+    char cnt_type[SMALL_BUF];
+    char buf[BUF_SIZE];
+    FILE* send_file;
+    
+    sprintf(cnt_type,"Content-type:%s\r\n\r\n",ct);
+    send_file=fopen(file_name,"r");
+    if(send_file == NULL){
+        send_error(fp);
+        return;
+    }
+    /*传输头信息*/
+    fputs(protocol,fp);
+    fputs(server,fp);
+    fputs(cnt_len,fp);
+    fputs(cnt_type,fp);
 
+    /*传输请求数据*/
+    while(fgets(buf,BUF_SIZE,send_file) != NULL){
+        fputs(buf,fp);
+        fflush(fp);
+    }
+    fflush(fp);
+    fclose(fp);
+}
 
+char* content_type(char* file){
+    char extension[SMALL_BUF];
+    char file_name[SMALL_BUF];
+    strcpy(file_name,file);
+    strtok(file_name,".");
+    strcpy(extension,strtok(NULL,"."));
+
+    if(!strcmp(extension,"html") || !strcmp(extension,"htm"))
+        return "text/html";
+    else
+        return "text/plain";
+}
+
+void send_error(FILE*fp){
+    char protocol[] = "HTTP/1.0 100 ok\r\n";
+    char server[] = "Server:Linux Web Server \r\n";
+    char cnt_len[] = "Content-length:2048\r\n";
+    char cnt_type[] = "Content-type:text/html\r\n\r\n";
+    char content[]="<html><head><title>NETWORK</title></head>"
+        "<body><font size=+5><br>发生错误！查看请求文件名和请求方式!"
+        "</font></body></html>";
+
+    fputs(protocol,fp);
+    fputs(server,fp);
+    fputs(cnt_len,fp);
+    fputs(cnt_type,fp);
+    fflush(fp);
+}
+void error_handling(const char* message){
+    fprintf(stderr,"%s\n",message);
+    exit(1);
+}
